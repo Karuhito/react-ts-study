5916758f8eb699d4ad28ed99eedfbcbfb09f571a
Karuhito <179291384+Karuhito@users.noreply.github.com>
2026-05-04 13:35:29 +0900

    2026-05-04 13:34 day8

--- phase-1-typescript/day8-generics/exercise.ts
+++ phase-1-typescript/day8-generics/exercise.ts
@@ -49,9 +49,26 @@ console.log(first?.location); // "国道1号"
 // Q1: PaginatedResponse<T> interface を定義する
 // 必要なプロパティ: data（T[]）, total（number）, page（number）, perPage（number）
 // TODO: interface PaginatedResponse<T> { ... }
+interface PaginatedResponse<T> {
+  data: T[],
+  total: number,
+  page: number,
+  perPage: number,
+}
 
 // この型を使って RoadReport のページネーション結果を定義する
 // TODO: const paginatedReports: PaginatedResponse<RoadReport> = { ... }
 // 期待: data に2件、total: 2、page: 1、perPage: 10
+const paginatedReports: PaginatedResponse<RoadReport> = {
+  data: [
+    { id: 1, location: "国道1号", condition: "良好" },
+    { id: 2, location: "国道2号", condition: "危険" },
+  ],
+  // data: reportsResponse.data, この記述法でも動くが今回は直接データを入れる
+  total: 2,
+  page: 1,
+  perPage: 10,
+}
 
+console.log(paginatedReports);
 export {};
