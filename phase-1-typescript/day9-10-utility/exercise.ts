bf75f63e04b25a0bdf47f5486dbd010a923a96ed
Karuhito <179291384+Karuhito@users.noreply.github.com>
2026-05-04 14:18:16 +0900

    2026-05-04 14:18 day9-10を学習

--- phase-1-typescript/day9-10-utility/exercise.ts
+++ phase-1-typescript/day9-10-utility/exercise.ts
@@ -42,11 +42,38 @@ console.log(newReport);
 
 // Q1: id・reportedAt・reporterName を除いた型を作る（Omit を使う）
 // TODO: type PublicReportData = ...
+type PublicReportData = Omit<RoadReport, "id" | "reportedAt" | "reporterName">;
 
 // Q2: location と condition だけを持つ型を作る（Pick を使う）
 // TODO: type MinimalReport = ...
-
+type MinimalReport =  Pick<RoadReport, "location" | "condition">;
 // Q3: 全プロパティが readonly な型を作る（Readonly を使う）
 // TODO: type FrozenReport = ...
+type FrozenReport = Readonly<RoadReport>;
 
+
+/* テストコード
+const publicData: PublicReportData = {
+  location: "国道1号",
+  condition: "良好",
+};
+
+const minimal: MinimalReport = {
+  location: "国道2号",
+  condition: "危険",
+};
+
+const frozen: FrozenReport = {
+  id: 1,
+  location: "国道3号",
+  condition: "悪化",
+  reportedAt: "2026-05-04",
+  reporterName: "田中太郎",
+};
+// frozen.id = 2;  Readonlyでプロパティの変更を制限
+
+console.log(publicData);
+console.log(minimal);
+console.log(frozen);
+*/
 export {};
